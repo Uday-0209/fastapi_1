53088fea2bfba986610587f6e573b2fa4eb484c0
Uday <udayhiremath02@gmail.com>
2025-07-16 21:14:24 +0530

    'new_update'

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, declarative_base
+#from sqlalchemy.ext.declarative import declarative_base
 
 #this url is used to create the location of the database on fastapi application.
 '''this is for sqlite3'''
